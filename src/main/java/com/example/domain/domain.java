334ab4ec439651b7ea1bbcc220524c4a93d37491
Kenta Suzuki <kenta.suzuki@rakus-partners.co.jp>
2022-05-11 23:47:33 +0900

    ドメインを作成

---
+++ src/main/java/com/example/domain/domain.java
@@ -0,0 +1,22 @@
+package com.example.domain;
+
+public class domain {
+	
+	private int num;
+
+	public int getNum() {
+		return num;
+	}
+
+	public void setNum(int num) {
+		this.num = num;
+	}
+
+	@Override
+	public String toString() {
+		return "domain [num=" + num + "]";
+	}
+	
+	
+
+}
