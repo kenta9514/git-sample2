38944b58b942501227236fb8485688de054ed093
Kenta Suzuki <kenta.suzuki@rakus-partners.co.jp>
2022-05-11 23:42:49 +0900

    first commit

---
+++ src/main/java/com/example/GitSample2Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSample2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/GitSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
